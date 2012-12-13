db5e7ce8568b0d81bd55aa969a1803f1b9ad102a
robinhwang <binbinhuangcsu@gmail.com>
2012-12-13 17:05:44 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RobinOffline::Application.config.secret_token = '5513594154994d09210f842759f6ec9139d8977c94e192e83ceb949b3cb9c5d1b74e051fb8beaf5a9bcc6c2b796226efaa9adf7fff078febfec45c3572153ffb'
